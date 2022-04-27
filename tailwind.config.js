03bb2b4c94c4044758f8c91f7630c4ea1e80b8e7
Antonio Lopez <pezlop202@gmail.com>
2022-04-26 17:18:14 -0700

    responsive and screenshot

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         lightGray: 'hsl(212, 45%, 89%)',
         grayishBlue: 'hsl(220, 15%, 55%)',
         darkBlue: 'hsl(218, 44%, 22%)',
+        bgBlue: '#d6e2f0',
       },
       fontFamily: {
         'sans-serif': ['Outfit'],
